7d8b8eabde63288b201fd89bb8e6274de08b8d69
Jay DeLuca <jaydeluca4@gmail.com>
2026-01-29 05:55:03 -0500

    Add collector sync workflow

---
+++ .github/scripts/use-cla-approved-bot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+# https://github.com/open-telemetry/community/blob/main/assets.md#otelbot
+git config user.name otelbot
+git config user.email 197425009+otelbot@users.noreply.github.com
